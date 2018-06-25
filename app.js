2fae9bf3c6a7ec2d0947f73212a95c0c1f20e2bc
greensike <reginald.stjulien@gmail.com>
2018-06-25 16:32:07 -0400

    added require('dotenv').config(); to app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+require('dotenv').config(); 
 var app = express();
 
 app.use(logger('dev'));
