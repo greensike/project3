fb3c7f6d9b9b0906b131a490a0173b6aa17d63d8
greensike <reginald.stjulien@gmail.com>
2018-06-26 10:28:34 -0400

    editing seeds.js

--- db/seeds.js
+++ db/seeds.js
@@ -4,3 +4,14 @@ const {Creature} = require('./schema.js')
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URI)
 const db = mongoose.connection;
+
+const saved = async () => {
+    await User.remove()
+    const John = new User({name: 'John', photo: 'enter photo source here'})
+    await John.save()
+    const Sarah = new User({name: 'Sarah', photo: 'enter photo source here'})
+    await Sarah.save()
+    db.close()
+  }
+  
+  saved()
