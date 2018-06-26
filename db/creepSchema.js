cc77bde970bf4c478c9d5baaf2ead7e0a08d86a9
greensike <reginald.stjulien@gmail.com>
2018-06-26 10:17:22 -0400

    editing creep schema

--- db/creepSchema.js
+++ db/creepSchema.js
@@ -1,3 +1,18 @@
 const mongoose = require('mongoose')
-
 const Schema = mongoose.Schema
+
+const creepSchema = new Schema({
+    name: String,
+    photo: String,
+    address: String,
+    age: Number,
+    description: String,
+    crime: String,
+    stars: String
+  })
+
+  const Creep = mongoose.model('Creep', creepSchema)
+
+module.exports = {
+  Creep
+}
