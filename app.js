e598eddaf4b074cdcbda12570b03497c90f381da
greensike <reginald.stjulien@gmail.com>
2018-06-25 23:03:37 -0400

    creating send bak

--- app.js
+++ app.js
@@ -27,10 +27,16 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('/', (req,res) => {
+    res.send("Hello World")
+})
 
 module.exports = app;
