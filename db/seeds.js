cb3311be2133f633e205dd26a370ced4fde2d411
greensike <reginald.stjulien@gmail.com>
2018-06-26 10:36:48 -0400

    editing seeds.js creep part

--- db/seeds.js
+++ db/seeds.js
@@ -11,9 +11,9 @@ const saved = async () => {
     await John.save()
     const Sarah = new User({name: 'Sarah', photo: 'enter photo source here'})
     await Sarah.save()
-    const Quagmire = new Creep({name: 'Quagmire', photo: 'enter photo source here',address: '123 Rhode Island',age: 34,name: 'weird ole giggidy giggidy',})
+    const Quagmire = new Creep({name: 'Quagmire', photo: 'enter photo source here', address: '123 Rhode Island', age: 34, description: 'weird ole giggidy giggidy', stars: '4'})
     await Quagmire.save()
-    const RedGuy = new Creep({name: 'Red Guy', photo: 'enter photo source here'})
+    const RedGuy = new Creep({name: 'Red Guy', photo: 'enter photo source here', address: '999 below ground', age: 1, description: 'red & butt naked', stars: '5',})
     await RedGuy.save()
     db.close()
   }
