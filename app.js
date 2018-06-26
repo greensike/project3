83943e1a10b0e2a76dc510290bb34a4cf67a3cc9
greensike <reginald.stjulien@gmail.com>
2018-06-25 22:41:14 -0400

    changed var to constn

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);  
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
