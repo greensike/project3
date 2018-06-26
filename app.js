bffcece80f6557dea7d9a7656d5918ed9d725cb4
greensike <reginald.stjulien@gmail.com>
2018-06-25 22:58:24 -0400

    added db checking in console.log

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ mongoose.Promise = global.Promise
 
 mongoose.connect(process.env.MONGODB_URI);  
 
+const db = mongoose.connection
+db.on('error', (error) => {
+    console.log(error)
+    console.log('you messed up')
+  })
+  db.once('open', () => {
+    console.log('Connected to MongoDB!')
+  })
+
 const app = express();
 app.use(logger('dev'));
 app.use(express.json());
