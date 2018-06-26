db14e8f130e23fc36fc993827df48eabd347729f
greensike <reginald.stjulien@gmail.com>
2018-06-26 13:26:07 -0400

    creep show route

--- routes/creepController.js
+++ routes/creepController.js
@@ -1,5 +1,7 @@
 
 var express = require('express');
 var router = express.Router();
+const router = express.Router({ mergeParams: true })
 
+const { User } = require('../db/userSchema.js')
 const { Creep } = require('../db/creepSchema.js')
