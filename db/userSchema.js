2e8cfe2e88c254134b6b78a35ee78166356df39a
greensike <reginald.stjulien@gmail.com>
2018-06-26 10:11:32 -0400

    editing user schema

--- db/userSchema.js
+++ db/userSchema.js
@@ -1,3 +1,13 @@
 const mongoose = require('mongoose')
-
 const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: String,
+    photo: String,
+  })
+
+  const User = mongoose.model('User', userSchema)
+
+module.exports = {
+  User
+}
