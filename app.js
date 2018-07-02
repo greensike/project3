43199da57700c3f1c5cec84f3dbf7d425b36cfc8
greensike <reginald.stjulien@gmail.com>
2018-07-02 12:42:55 -0400

    deploying heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/userController');
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(cookieParser());
 
 
 const db = mongoose.connection
